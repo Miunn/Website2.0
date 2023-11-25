804bcf32927fbc7be9a991637d6d8db9a2b81332
Miunn <remcaulier@orange.fr>
2023-11-25 21:09:43 +0100

    Selector component

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,21 @@
 import '../css/home.scss';
+
+const tabSelects = document.querySelectorAll('.tab-select');
+
+tabSelects.forEach(b => {
+    b.addEventListener('click', (e) => {
+        let btn = e.target;
+
+        let currentBtn = btn.closest('.tab-selector').querySelector('.tab-select.current');
+        currentBtn.classList.remove('current');
+        btn.classList.add('current');
+
+        let relatedTabsContainer = document.querySelector(btn.dataset.tabContainer);
+        console.log(btn.dataset.tabContainer);
+        console.log(relatedTabsContainer);
+        let currentTab = relatedTabsContainer.querySelector('.tab.current');
+
+        currentTab.classList.remove('current');
+        relatedTabsContainer.querySelector(btn.dataset.tab).classList.add('current');
+    });
+});
